31857503a15430cd517b0843ee75b9cc4f8cdf5f
VayoreM <vayore.mhlanga@praesignis.co.za>
2021-10-22 12:40:22 +0200

    all

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -18,5 +18,14 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::post('login', 'AuthController@login');
 
-Route::apiResource('users',UserController::class);
+Route::group(['middleware' => 'auth:api'], function (){
+    Route::apiResource('users','UserController');
+});
+
+Route::get('users','UserController@index');
+Route::get('users/{id}','UserController@show');
+Route::post('users','UserController@store');
+Route::put('users/{id}','UserController@update');
+Route::delete('users/{id}','UserController@destroy');
